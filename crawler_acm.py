b6504d6da05d7729d0b75642980d9c3c0bbd3955
sirius <sirius.ustc@gmail.com>
2019-05-30 20:35:26 +0800

    repair the bug of id and authors

--- crawler_acm.py
+++ crawler_acm.py
@@ -1,29 +1,28 @@
 import feedparser
 from bs4 import BeautifulSoup
-from urllib.parse import urlencode, parse_qs, urlparse
+from urllib.parse import urlencode, parse_qs, urlparse, urlunparse
 from urllib.request import urlopen, install_opener, build_opener
 from time import sleep
 
-opener=build_opener()
-opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
-install_opener(opener)
-def get_paper_list(key=None, max_result=20):
+bs_feature = "lxml"
+def get_paper_list(key=None, max_results=20):
     # url_string = 'http://portal.acm.org/browse_dl.cfm?linked=1&part=series&coll=portal&dl=ACM&CFID=81659906&CFTOKEN=15682890'
     # url_string = https://dl.acm.org/results.cfm?query=learning&start=0
+    opener=build_opener()
+    opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+    install_opener(opener)
     cnt = 0
     paper_url = {}
-    while cnt*20 < max_result:
+    while cnt*20 < max_results:
         url_string = 'https://dl.acm.org/results.cfm?'+urlencode({"query": key, "start": cnt*20})
-        print(url_string)
         res = urlopen(url_string).read()
-        soup = BeautifulSoup(res, features="lxml")
+        soup = BeautifulSoup(res, features=bs_feature)
         papers = []
         paper_list = soup.find_all("div", class_="title")
         for i in paper_list:
             papers.append(i.find_all("a")[0])
         for k in papers:
             paper_url[k.getText()] = 'https://dl.acm.org/'+k.get('href', None)
-        print(paper_url)
         sleep(0.5)
         cnt += 1
     return paper_url
@@ -31,18 +30,21 @@ def get_paper_list(key=None, max_result=20):
 def get_paper_info(url=None, title=None):
     if url == None:
         return {}
+    opener=build_opener()
+    opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+    install_opener(opener)
     paper_info = {}
     paper_info['title'] = title
     arg = urlparse(url).query
     args = parse_qs(arg)
-    urlargs = urlencode({"id": args['id'], "usebody": "tabbody"})
+    urlargs = urlencode({"id": args['id'][0], "usebody": "tabbody"})
     tmp = urlopen('https://dl.acm.org/tab_abstract.cfm?'+urlargs)
     abstract = tmp.read()
-    paper_info['abstract'] = BeautifulSoup(abstract).text
-    tmp = urlopen('https://dl.acm.org/tab_comments.cfm?'+urlargs)
+    paper_info['abstract'] = BeautifulSoup(abstract, features=bs_feature).text
+    tmp = urlopen('https://dl.acm.org/tab_authors2.cfm?'+urlargs)
     author_list = []
     authors = tmp.read()
-    authors_part = BeautifulSoup(authors).find_all('strong')
+    authors_part = BeautifulSoup(authors, features=bs_feature).find_all('strong')
     for i in authors_part:
         tmp = i.find_all('a')
         if len(tmp) == 0:
@@ -54,7 +56,7 @@ def get_paper_info(url=None, title=None):
     paper_info['authors'] = author_list
     tmp = urlopen('https://dl.acm.org/tab_comments.cfm?'+urlargs)
     comment_list = []
-    comment = tmp.read()
+    comment = BeautifulSoup(tmp.read(), features=bs_feature)
     comments = comment.find_all('p')
     for i in comments:
         comment_list.append(i.getText())
@@ -62,4 +64,16 @@ def get_paper_info(url=None, title=None):
 
     return paper_info
 
-# def acm_crawler(key=None, max_results=None):
+def crawler(key=None, max_results=None):
+    paper_list = get_paper_list(key, max_results)
+    print(paper_list)
+    paper_infos = []
+    cnt = 0
+    for title in paper_list:
+        if cnt >= max_results:
+            break
+        elif cnt % 20 == 0 and cnt >= 20:
+            sleep(0.5)
+        paper_infos.append(get_paper_info(paper_list[title], title))
+        cnt += 1
+    return paper_infos
